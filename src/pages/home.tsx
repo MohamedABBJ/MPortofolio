e9511bf277b080e8d123c17a7bd2d0d70c5cf87f
unknown <mohamedabbj@gmail.com>
2024-07-26 16:46:00 -0400

    initial commit

---
+++ src/pages/home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="h-lvh bg-black flex items-center justify-center flex-col gap-12">
+      <p className="text-5xl text-blue-500 font-bold">Mohamed Badwan</p>
+      <div className="w-48 h-48 rounded-full drop-shadow-[0_0px_30px_#ffffff] bg-white" />
+    </div>
+  );
+}
